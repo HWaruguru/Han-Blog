339c7b2c8bc592736ceb1b481817e5cf9e482a2c
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-26 00:09:24 +0300

    Add blog cards

--- app/views.py
+++ app/views.py
@@ -2,10 +2,44 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
+@app.route("/")
 def index():
+    blogs = [
+        {
+            "title": "When the biggest Disruption is to just sit down and focus",
+            "content": "If I had to distill a singular takeaway from the hours of programming, demo floor and Startup Battlefield, it would be this: Disruption needs direction. We’re in the middle of unprecedented times, and while that’s been good news for some entrepreneurs (and bad news for very many), focus may be what leads us out of it.",
+            "author": "Natasha Mascarenhas",
+            "date": "September 25, 2021",
+            "heart": 1
+        },
+        {
+            "title": "When the biggest Disruption is to just sit down and focus",
+            "content": "If I had to distill a singular takeaway from the hours of programming, demo floor and Startup Battlefield, it would be this: Disruption needs direction. We’re in the middle of unprecedented times, and while that’s been good news for some entrepreneurs (and bad news for very many), focus may be what leads us out of it.",
+            "author": "Natasha Mascarenhas",
+            "date": "September 25, 2021",
+            "heart": 1
+        },
+        {
+            "title": "When the biggest Disruption is to just sit down and focus",
+            "content": "If I had to distill a singular takeaway from the hours of programming, demo floor and Startup Battlefield, it would be this: Disruption needs direction. We’re in the middle of unprecedented times, and while that’s been good news for some entrepreneurs (and bad news for very many), focus may be what leads us out of it.",
+            "author": "Natasha Mascarenhas",
+            "date": "September 25, 2021",
+            "heart": 1
+        },
+        {
+            "title": "When the biggest Disruption is to just sit down and focus",
+            "content": "If I had to distill a singular takeaway from the hours of programming, demo floor and Startup Battlefield, it would be this: Disruption needs direction. We’re in the middle of unprecedented times, and while that’s been good news for some entrepreneurs (and bad news for very many), focus may be what leads us out of it.",
+            "author": "Natasha Mascarenhas",
+            "date": "September 25, 2021",
+            "heart": 1
+        },
+        {
+            "title": "When the biggest Disruption is to just sit down and focus",
+            "content": "If I had to distill a singular takeaway from the hours of programming, demo floor and Startup Battlefield, it would be this: Disruption needs direction. We’re in the middle of unprecedented times, and while that’s been good news for some entrepreneurs (and bad news for very many), focus may be what leads us out of it.",
+            "author": "Natasha Mascarenhas",
+            "date": "September 25, 2021",
+            "heart": 1
+        }
+    ]
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('index.html')
+    return render_template("index.html", blogs=blogs)
